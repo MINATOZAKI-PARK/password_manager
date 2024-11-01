6394666db75ca5610d96f658999a41dbce41188b
Minatozaki <148259896+MINATOZAKI-PARK@users.noreply.github.com>
2024-11-01 14:53:10 -0600

    codigo comenta

--- generator.py
+++ generator.py
@@ -5,23 +5,28 @@ def generate_password(long):
     characters = sg.ascii_letters + sg.digits + sg.punctuation # conjunto de caractertes
     try:
         if long >= 8: # Condicional para que la longitud no sea menor a 8
+
             while True:
                 password = "".join(ss.choice(characters)for _ in range(long)) # Generador de contraseña
                 if(any(char in sg.ascii_letters for char in password) and # Verifica si la contraseña tiene al menos un caracter de cada uno
                     any(char in sg.punctuation for char in password) and
                     any(char in sg.digits for char in password)):
                     return password
+        
         else:
             raise ValueError("La longitud mínima recomendada es de 8 caracteres.") # Si se ingresa una longitud menor a 8 lanza este error
         
     except ValueError as e:
-        print("Error: ", e)
+        print("Error:", e)
+        return None  # Retorna None en caso de error
 
 
 if __name__ == "__main__": # codigo solo para generator.py
+
     try:
         length = int(input("Ingrese la longitud de la contraseña: "))
         print("Contraseña generada:", generate_password(length))
-    except ValueError as e:
-        print("Error:", e)
+
+    except ValueError: # Error en caso de ingresasr algo que no sea un numero
+        print("Error: Ingrese un número válido para la longitud.")
 
