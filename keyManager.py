fdb2e4d97fe7f7f8710abb091a7bd7ffee7986c5
Minatozaki <148259896+MINATOZAKI-PARK@users.noreply.github.com>
2024-11-03 15:58:30 -0600

    Cambio insignificante

--- keyManager.py
+++ keyManager.py
@@ -2,14 +2,17 @@ from cryptography.fernet import Fernet
 
 def generateKey(clave= "clave.key"): # Parametro predefinido para no ingresa un nombre
     key = Fernet.generate_key()  # genera la key 
+
     with open(clave , "wb") as key_file: # Crea un archivo para escribir la key 
         key_file.write(key) # Eacribe la Key en el archivo
+
     print(f"Clave generada y guardada en {clave}")
 
 
 
 def loadKey(clave= "clave.key"):
     try:
+        
         with open(clave, "rb") as key_file: # abre el arcivo
             key = key_file.read() # lee el archivo 
         return key # lo retorna 
